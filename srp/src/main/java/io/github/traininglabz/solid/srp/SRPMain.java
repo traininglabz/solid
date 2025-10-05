38ed688115a127a98a9ca76c5dc1e02292a679ca
rittikeghosh <RittikeGhosh@users.noreply.github.com>
2025-10-05 08:25:32 +0530

    init

---
+++ srp/src/main/java/io/github/traininglabz/solid/srp/SRPMain.java
@@ -0,0 +1,7 @@
+package io.github.traininglabz.solid.srp;
+
+public class SRPMain {
+    public static void main(String[] args) {
+        System.out.println("SRP: The Single Responsibility Principle");
+    }
+}
